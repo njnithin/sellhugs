b03e421d01a828eee150fc3438efaa5efc46b367
Nithin <n.j.nithin7@gmail.com>
2020-08-16 10:22:52 +0530

    Config changed

--- src/components/Learn.vue
+++ src/components/Learn.vue
@@ -1,6 +1,7 @@
 <template>
 	<v-content class="learn-content">
-		<h1> Homepage  </h1>
+		<h1 class="blue--text"> Homepage  </h1>
+		<v-btn depressed flat class="pink"> Login</v-btn>
 
 	</v-content>
 </template>
